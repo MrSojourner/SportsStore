acefc4458681de39027fab917c0d906258c89ac3
Ilya G <ilyagainulin@gmail.com>
2019-01-15 17:06:36 +0300

    Process of paging

--- SportsStore/Program.cs
+++ SportsStore/Program.cs
@@ -19,7 +19,8 @@ namespace SportsStore
 
         public static IWebHost BuildWebHost(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
-                .UseStartup<Startup>()
+                .UseStartup<Startup>().UseDefaultServiceProvider(options =>
+                options.ValidateScopes = false)
                 .Build();
     }
 }
